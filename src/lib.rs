576be3ff9e2e27aad722df815320cb1be28838b8
LMH01 <louis.haensch01@gmail.com>
2021-12-02 20:56:19 +0100

    Rewrote read_file function to return an result instead of panicing. Also changed version so 1.0.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,39 @@
 use std::{
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Error},
     path::Path,
 };
 
-/// Reads the input file into a vector containing a new entry per line
-///
-/// The end of line character is not writtten in the entry
+/// Reads the input file into a vector containing a new entry per line and returns it.
+/// 
+/// If the file could not be opened an error is returned.
+/// The end of line character is not writtten in the entry.
 ///
 /// # Arguments
 ///
 /// * `file_name` - A string slice containing the file name
 ///
-/// # Panics
-///
-/// Panics when the file does not exist
-///
 /// # Examples
 ///
 /// ```
 /// use adventofcode_lmh01_lib::read_file;
-/// let content = read_file("test_file.txt");
+///
+/// let vec = match read_file("test_file.txt") {
+///     Ok(ok) => ok,
+///     Err(err) => {
+///         println!("Unable to read file: {}", err);
+///         std::process::exit(0);
+///     }
+/// };
+///
 /// ```
-pub fn read_file(file_name: &str) -> Vec<String> {
+pub fn read_file(file_name: &str) -> Result<Vec<String>, Error> {
     let mut content: Vec<String> = Vec::new();
     let path = Path::new(file_name);
     let file = File::open(path);
     let file = match file {
         Ok(file) => file,
-        Err(error) => panic!("Problem while opening file: {}", error),
+        Err(error) => return Err(error),
     };
     let mut buffered_reader = BufReader::new(file);
     let mut current_line: String = String::new();
@@ -36,7 +41,7 @@ pub fn read_file(file_name: &str) -> Vec<String> {
         content.push(current_line.trim().to_string());
         current_line = String::new();
     }
-    content
+    Ok(content)
 }
 
 #[cfg(test)]
@@ -44,20 +49,10 @@ mod tests {
     use crate::read_file;
 
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-
-    #[test]
     fn read_file_() {
         let content = read_file("test_file.txt");
-        assert_eq!(content.len(), 10);
-    }
-
-    #[test]
-    #[should_panic]
-    fn read_file_panic() {
-        let _content = read_file("does_not_exist.txt");
+        assert_eq!(content.unwrap_or(Vec::new()).len(), 10);
+        let content = read_file("does_not_exist.txt");
+        assert!(content.is_err());
     }
 }
